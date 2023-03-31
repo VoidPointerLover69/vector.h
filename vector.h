daa36fda9f955b54538d59b0c225ad6cf1ed824d
VoidPointerLover69 <82274315+VoidPointerLover69@users.noreply.github.com>
2023-03-31 23:57:54 +0200

    Update vector.h
    code now works when you define multiple vectors of same type

--- vector.h
+++ vector.h
@@ -5,33 +5,33 @@
 #include <string.h>
 #include <stdio.h>
 
-#define newVector(vec, t) \
+#define newVector(vect, t) \
 typedef struct {\
     t* arr;\
     size_t size;\
     size_t memsize;\
     char* type;\
-} vector_##t;\
-void clean_vec_##t(vector_##t** vec) {\
+} vector_##vect;\
+void clean_vec_##vect(vector_##vect** vec) {\
     free((*vec)->arr);\
     free((*vec)->type);\
     free(*vec);\
 }\
-vector_##t* __attribute__((__cleanup__(clean_vec_##t))) vec = malloc(sizeof(vector_##t));\
-vec->arr = malloc(16);\
-vec->size = 0;\
-vec->memsize = 16;\
-vec->type = malloc(strlen(#t)+1);\
-strcpy(vec->type, #t);
+vector_##vect* __attribute__((__cleanup__(clean_vec_##vect))) vect = malloc(sizeof(vector_##vect));\
+vect->arr = malloc(16);\
+vect->size = 0;\
+vect->memsize = 16;\
+vect->type = malloc(strlen(#t)+1);\
+strcpy(vect->type, #t);
 
-#define push_back(vec, t) \
+#define push_back(vec, T) \
 if (vec->memsize >= vec->size) {\
-    size_t new_memsize = vec->memsize+sizeof(t)+8;\
+    size_t new_memsize = vec->memsize+sizeof(T)+8;\
     void* new_arr = realloc(vec->arr, new_memsize);\
     vec->arr = new_arr;\
     vec->memsize = new_memsize;\
 }\
-vec->arr[vec->size] = t;\
+vec->arr[vec->size] = T;\
 vec->size++;
 
 
