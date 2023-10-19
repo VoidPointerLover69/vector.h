d84f03ad010925efe44107c3ba4743f390c7f7dc
VoidPointerLover69 <82274315+VoidPointerLover69@users.noreply.github.com>
2023-10-19 21:25:00 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,21 +1,37 @@
 #include "vector.h"
+#include <stdio.h>
+
+typedef char* string; //only works like this because of the pointer character
+declVector(string);
+
+void printVector(vector_string* vec) {
+	for(int i = 0; i < vecSize(vec); i++) {
+		printf("%s ", vecIndex(vec, i));
+	}
+}
 
 int main() {
-    newVector(a, char*);
-    push_back(a, "hello");
-    push_back(a, ", ");
-    push_back(a, "world!");
-    newVector(b, char*);
-    vecCopy(b, a);
+	newVector(a, string);
+	
+	push_front(a, "Hello");
+	push_back(a, "...");
+	push_back(a, "World!\n");
+	push_front(a, "...");
+	
+	printVector(a);
+	
+	vecPop(a, vecSize(a)-1);
+	printVector(a);
+	
+	newVector(b, string);
+	vecCopy(b, a);
+	push_back(b, "World");
+	printVector(b);
 
-    printf("%s", vecIndex(b, 0));
-    printf("%s", vecIndex(b, 1));
-    printf("%s\n", vecIndex(b, 2));
-    vecPop(b, 1);
-    printf("%s", vecIndex(b, 0));
-    printf("%s", vecIndex(b, 1));
-    return 0;
+	return 0;
 }
-//output should be 
-//hello, world!
-//helloworld!
+/*
+... Hello ... World!
+ ... Hello ... ... Hello ... World
+ 
+this should be the output*/
