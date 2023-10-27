c45dd68b83612b50f804e760f189bc7e446ddb0a
VoidPointerLover69 <82274315+VoidPointerLover69@users.noreply.github.com>
2023-10-27 13:37:15 +0200

    vector 2.1
    added some stuff i guess and now its optimized

--- vector.h
+++ vector.h
@@ -10,8 +10,9 @@
 	typedef struct {\
 		t* arr;\
 		size_t size;\
-		size_t memsize;\
+		size_t capacity;\
 	} vector__##t;\
+	\
 	void clean_vec_##t(vector__##t** vec) {\
 		free((*vec)->arr);\
 		free(*vec);\
@@ -21,112 +22,117 @@
 //using attribute cleanup to automatically free memory 
 #define newVector(vect, t) \
 	vector__##t* __attribute__((__cleanup__(clean_vec_##t))) vect = malloc(sizeof(vector_##t));\
-	vect->arr = malloc(16);\
+	vect->arr = malloc(sizeof(t)*8);\
 	vect->size = 0;\
-	vect->memsize = 16;
+	vect->capacity = sizeof(t)*8;
 
 //a variadic macro, using typeof() and __VA_ARGS__ to create an array of arguments given and pushes infrost of vector
-#define push_front(vec, ...) \
-{\
-	typeof(*(vec->arr)) lol[] = {__VA_ARGS__};\
+#define push_front(vec, ...) {\
+	vecType(vec) lol[] = {__VA_ARGS__};\
 	size_t lols = sizeof(lol)/sizeof(lol[0]);\
-	if (vec->memsize <= vec->size) {\
-		size_t new_memsize = vec->memsize+lols+sizeof(lol[0]);\
-		void* new_arr = realloc(vec->arr, new_memsize);\
-		vec->arr = new_arr;\
-		vec->memsize = new_memsize;\
+	\
+	if (vec->capacity <= vec->size) {\
+		size_t new_capacity = vec->capacity+lols+sizeof(lol[0]);\
+		vec->arr = realloc(vec->arr, new_capacity);\
+		vec->capacity = new_capacity;\
 	}\
-	for (int i = 0; i < lols; i++) {\
-		for (int j = vec->size+i; j > 0; j--) {\
-			vec->arr[j] = vec->arr[j-1];\
-		}\
+	\
+	if (vec->size > 0) {\
+		memmove(vec->arr+lols, vec->arr, vec->size*sizeof(vecType(vec)));\
+		memmove(vec->arr, lol, sizeof(lol));\
 	}\
-	for (int i = 0; i < lols; i++) {\
-		vec->arr[i] = lol[i];\
+	else {\
+		memmove(vec->arr, lol, sizeof(lol));\
 	}\
 	vec->size += lols;\
 }
 
 //same as push_front but it pushes back
-#define push_back(vec, ...) \
-{\
-	typeof(*(vec->arr)) lol[] = {__VA_ARGS__};\
+#define push_back(vec, ...) {\
+	vecType(vec) lol[] = {__VA_ARGS__};\
 	size_t lols = sizeof(lol)/sizeof(lol[0]);\
-	if (vec->memsize <= vec->size+1) {\
-		size_t new_memsize = vec->memsize+lols+sizeof(lol[0]);\
-		void* new_arr = realloc(vec->arr, new_memsize);\
-		vec->arr = new_arr;\
-		vec->memsize = new_memsize;\
+	\
+	if (vec->capacity <= vec->size+1) {\
+		size_t new_capacity = vec->capacity+lols+sizeof(lol[0]);\
+		vec->arr = realloc(vec->arr, new_capacity);\
+		vec->capacity = new_capacity;\
 	}\
-	for (int i = vec->size; i < lols+vec->size; i ++) {\
-		vec->arr[i] = lol[i-vec->size];\
-	}\
+	\
+	memmove(vec->arr+vec->size, lol, sizeof(lol));\
 	vec->size += lols;\
 }
 
 //concatenates two vectors 
 #define vecCat(dest, source) \
-	if (dest->memsize < dest->size + source->size) {\
+	if (dest->capacity < dest->size + source->size) {\
 		dest->arr = realloc(dest->arr, dest->size + source->size + sizeof(*source->arr));\
-		dest->memsize = source->size + sizeof(*source->arr);\
+		dest->capacity = source->size + sizeof(*source->arr);\
 	}\
-	for (int i = dest->size; i <  dest->size + source->size; i++) {\
-		dest->arr[i] = source->arr[i - dest->size]; \
-	}\
+	\
+	memmove(dest->arr+dest->size, source->arr, sizeof(vecType(dest))*source->size);\
 	dest->size += source->size;\
 
 //pushes source infront of dest 
 #define vecCatfr(dest, source) \
-	if ((dest->size + source->size) > dest->memsize) { \
+	if ((dest->size + source->size) > dest->capacity) { \
 		dest->arr = realloc(dest->arr, dest->size + source->size + sizeof(*dest->arr));\
-		dest->memsize = dest->size + source->size + sizeof(*dest->arr); \
+		dest->capacity = dest->size + source->size + sizeof(*dest->arr); \
 	}\
-	for (int i = 0; i < dest->size; i++) {\
-		dest->arr[i+source->size] = dest->arr[i]; \
-	} \
-	for (int i = 0; i < source->size; i++) { \
-		dest->arr[i] = source->arr[i]; \
-	} \
+	\
+	memmove(dest->arr+source->size, dest->arr, dest->size*sizeof(vecType(dest)));\
+	\
+	memmove(dest->arr, source->arr, source->size*sizeof(vecType(dest)));\
 	dest->size += source->size; \
 
 //swaps two vectors
 #define vecSwap(vec1, vec2) \
-	if (vec1->memsize < vec2->memsize)\
-		vec1->arr = realloc(vec1->arr, vec2->memsize+sizeof(*vec1->arr));\
-	else if (vec1->memsize > vec2->memsize)\
-		vec2->arr = realloc(vec2->arr, vec1->memsize+sizeof(*vec1->arr));\
+	if (vec1->capacity < vec2->capacity)\
+		vec1->arr = realloc(vec1->arr, vec2->capacity+sizeof(*vec1->arr));\
+	else if (vec1->capacity > vec2->capacity)\
+		vec2->arr = realloc(vec2->arr, vec1->capacity+sizeof(*vec1->arr));\
+	\
 	{\
-	void* idk = malloc(vec1->memsize);\
-	memcpy(idk, vec1->arr, vec1->size*sizeof(vec1->arr[0]));\
-	memcpy(vec1->arr, vec2->arr, vec2->size*sizeof(vec1->arr[0])); \
-	memcpy(vec2->arr, idk, vec1->size*sizeof(vec1->arr[0]));\
-	size_t bug = vec1->size;\
-	vec1->size = vec2->size;\
-	vec2->size = bug;\
-	free(idk);\
-	}
-
+		vecType(vec1)* idk = malloc(vec1->capacity);\
+		\
+		memcpy(idk, vec1->arr, vec1->size*sizeof(vec1->arr[0]));\
+		\
+		memcpy(vec1->arr, vec2->arr, vec2->size*sizeof(vec1->arr[0])); \
+		\
+		memcpy(vec2->arr, idk, vec1->size*sizeof(vec1->arr[0]));\
+		\
+		size_t bug = vec1->size;\
+		vec1->size = vec2->size;\
+		vec2->size = bug;\
+		free(idk);\
+	}\
+			
 #define vecIndex(vec, ind) vec->arr[ind]
 
 #define vecSize(vec) vec->size
 
-#define vecMemsize(vec) vec->memsize
+#define vecCapacity(vec) vec->capacity
 
 #define vecArr(vec) vec->arr
 
-#define vecPop(vec, ind) \
-	if (vec->size != 0) {\
-		for(int i = ind; i < vec->size; i++) {\
-			vec->arr[i] = vec->arr[i+1];\
-		}\
+#define vecType(vec) typeof(*(vec->arr))
+
+#define vecClear(vec) {\
+	memset(vec->arr, NULL, vec->size);\
+	vec->size = 0;\
+}
+
+#define vecPop(vec, ind) {\
+	if(ind != 0 && ind <= vec->size && vec->size != 0) {\
+		memmove(vec->arr+ind, vec->arr+ind+1, (vec->size-ind)*sizeof(vecType(vec)));\
 		vec->size--;\
-	}
+	}\
+}	
 //as far as I've tested it it works though it might not be such a good idea but memcpy was a bitch so it will do until i find a bug
 
 #define vecCopy(vec1, vec2)\
-	if (vec2->memsize > vec1->memsize) {\
-		vec1->memsize = vec2->memsize;\
-		vec1->arr = realloc(vec1->arr, vec1->memsize);\
+	if (vec2->capacity > vec1->capacity) {\
+		vec1->capacity = vec2->capacity;\
+		vec1->arr = realloc(vec1->arr, vec2->capacity);\
 	}\
 	memcpy(vec1->arr, vec2->arr, vec2->size*sizeof(vec2->arr[0]));\
 	vec1->size = vec2->size;   
